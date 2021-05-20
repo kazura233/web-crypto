bf2f0c56af964e921c6835082ccdc40719dd8aa9
kazura233 <kazira233@gmail.com>
2021-05-20 11:15:10 +0800

    build

--- src/aes-128-ecb.ts
+++ src/aes-128-ecb.ts
@@ -5,9 +5,7 @@ const Aes128Ecb = {
     return CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(message), CryptoJS.enc.Utf8.parse(key), {
       mode: CryptoJS.mode.ECB,
       padding: CryptoJS.pad.Pkcs7,
-    })
-      .toString(CryptoJS.format.OpenSSL)
-      .replace(/\s/g, '')
+    }).toString(CryptoJS.format.OpenSSL)
   },
   decrypt(ciphertext: string, key: string) {
     return CryptoJS.AES.decrypt(ciphertext.replace(/\s/g, ''), CryptoJS.enc.Utf8.parse(key), {
--- src/base64.ts
+++ src/base64.ts
@@ -2,7 +2,7 @@ import CryptoJS from 'crypto-js'
 
 const Base64 = {
   stringify(str: string): string {
-    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(str))
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(str.replace(/\s/g, '')))
   },
   parse(str: string): string {
     return CryptoJS.enc.Base64.parse(str).toString(CryptoJS.enc.Utf8)

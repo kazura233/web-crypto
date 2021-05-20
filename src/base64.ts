3e00358f917a813ed12e5e9587e992dd7ca8ebea
kazura233 <kazira233@gmail.com>
2021-05-20 15:42:06 +0800

    build

--- src/base64.ts
+++ src/base64.ts
@@ -2,10 +2,10 @@ import CryptoJS from 'crypto-js'
 
 const Base64 = {
   stringify(str: string): string {
-    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(str.replace(/\s/g, '')))
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(str))
   },
   parse(str: string): string {
-    return CryptoJS.enc.Base64.parse(str).toString(CryptoJS.enc.Utf8)
+    return CryptoJS.enc.Base64.parse(str.replace(/\s/g, '')).toString(CryptoJS.enc.Utf8)
   },
 }
 
